21d07fa0783688600997d2972b37bd4b027056cf
Tony Tiratay <tony.tiratay@gmail.com>
2016-01-05 01:52:14 +0100

    First page

---
+++ client/init.js
@@ -0,0 +1,18 @@
+
+  // counter starts at 0
+Session.setDefault('counter', 0);
+  
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+Template.hello.helpers({
+
+});
+
+Template.hello.events({
+    'click button': function () {
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
+    }
+});
